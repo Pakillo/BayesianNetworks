05ef6723b4552f48ff35d3da9820cf222d1fe47b
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-05-05 22:10:06 +0200

    add function to check model

---
+++ R/check_model.R
@@ -0,0 +1,39 @@
+#' Check fitted model
+#'
+#' @param fit A fitted model, as obtained from `fit_model()`.
+#'
+#' @return Model checks on console and graphical window.
+#' @export
+#' @import cmdstanr
+#'
+#' @examples
+#' data(web)
+#' dt <- prepare_data(mat = web, sampl.eff = rep(20, nrow(web)))
+#' fit <- fit_model(dt)
+#' check_model(fit)
+
+check_model <- function(fit = NULL) {
+
+  fit$diagnostic_summary()
+
+  fit$cmdstan_diagnose()
+
+  check_lp(fit)
+
+}
+
+
+# Check log posterior across chains
+check_lp <- function(fit = NULL) {
+  lp <- fit$draws("lp__", format = "df")
+
+  if (requireNamespace("ggplot2")) {
+    ggplot2::ggplot(lp, ggplot2::aes(.draw, lp__, colour = as.factor(.chain))) +
+      ggplot2::geom_point() +
+      ggplot2::theme_minimal() +
+      ggplot2::labs(title = "Log posterior across chains")
+  } else {
+    plot(lp$.draw, lp$lp__)
+  }
+
+}

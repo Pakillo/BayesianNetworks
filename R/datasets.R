8d9751ff288126f891255200a82814f017bd367a
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-10 12:48:43 +0200

    document example dataset

---
+++ R/datasets.R
@@ -0,0 +1,12 @@
+#' Plant-pollinator network
+#'
+#' An example bipartite network of 8 plant species and 21 pollinators,
+#' from [Kaiser-Bunbury et al. 2017](https://doi.org/10.1038/nature21071).
+#'
+#' @format ## `web`
+#' A numeric (integer) matrix with 8 rows (representing plants) and 21 columns
+#' (representing animals)
+#' @source Kaiser-Bunbury, C., Mougal, J., Whittington, A. et al.
+#' Ecosystem restoration strengthens pollination network resilience and function.
+#' Nature 542, 223–227 (2017). https://doi.org/10.1038/nature21071
+"web"
